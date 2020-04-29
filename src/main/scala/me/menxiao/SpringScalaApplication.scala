e6b0676d06233d88ca51904a26f51869a232777b
Menghua Xiao <menxiao@microsoft.com>
2020-04-29 22:04:44 +0800

    Starter

---
+++ src/main/scala/me/menxiao/SpringScalaApplication.scala
@@ -0,0 +1,11 @@
+package me.menxiao
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringScalaApplication
+
+object SpringScalaApplication extends App {
+  SpringApplication.run(classOf[SpringScalaApplication])
+}
---
+++ src/test/java/me/menxiao/SpringScalaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.menxiao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
