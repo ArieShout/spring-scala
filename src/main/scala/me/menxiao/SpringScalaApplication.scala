b208103f48a3b0c2d7b9bb10c2065af315e20e48
Menghua Xiao <arieshout@gmail.com>
2020-05-01 12:37:34 +0800

    Move swagger configurations to a single class

--- src/main/scala/me/menxiao/SpringScalaApplication.scala
+++ src/main/scala/me/menxiao/SpringScalaApplication.scala
@@ -2,25 +2,9 @@ package me.menxiao
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.web.bind.annotation.RestController
-import springfox.documentation.builders.{PathSelectors, RequestHandlerSelectors}
-import springfox.documentation.spi.DocumentationType
-import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
-@EnableSwagger2
-class SpringScalaApplication {
-  @Bean def api(): Docket = {
-    new Docket(DocumentationType.SWAGGER_2)
-      .select()
-      .apis(RequestHandlerSelectors.withClassAnnotation(classOf[RestController]))
-      .paths(PathSelectors.any())
-      .build()
-      .genericModelSubstitutes(classOf[Option[_]])
-  }
-}
+class SpringScalaApplication
 
 object SpringScalaApplication extends App {
   SpringApplication.run(classOf[SpringScalaApplication], args: _*)
---
+++ src/main/scala/me/menxiao/config/SwaggerConfiguration.scala
@@ -0,0 +1,66 @@
+package me.menxiao.config
+
+;
+
+import java.util
+
+import com.fasterxml.classmate.TypeResolver
+import org.springframework.context.annotation.{Bean, Configuration}
+import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.builders.{PathSelectors, RequestHandlerSelectors}
+import springfox.documentation.schema.{AlternateTypeRule, AlternateTypeRuleConvention, AlternateTypeRules, WildcardType}
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+import scala.collection.mutable
+import scala.jdk.CollectionConverters._
+import scala.reflect._
+import scala.reflect.runtime.universe._
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfiguration {
+  @Bean def api(): Docket = {
+    new Docket(DocumentationType.SWAGGER_2)
+      .select()
+      .apis(RequestHandlerSelectors.withClassAnnotation(classOf[RestController]))
+      .paths(PathSelectors.any())
+      .build()
+      .genericModelSubstitutes(classOf[Option[_]])
+  }
+
+  @Bean def alternativeTypeRules(typeResolver: TypeResolver): AlternateTypeRuleConvention = {
+    new AlternateTypeRuleConvention {
+      override def rules(): util.List[AlternateTypeRule] =
+        Seq(
+          rule[Iterator[_], util.Iterator[_]],
+          rule[Iterable[_], java.lang.Iterable[_]],
+          rule[mutable.Buffer[_], util.List[_]],
+          rule[mutable.Set[_], util.Set[_]],
+          rule[Seq[_], util.List[_]],
+          rule[mutable.Seq[_], util.List[_]],
+          rule[Set[_], util.Set[_]],
+          rule[mutable.Map[_, _], util.Map[_, _]],
+          rule[collection.concurrent.Map[_, _], util.concurrent.ConcurrentMap[_, _]],
+          rule[Map[_, _], util.Map[_, _]]
+        ).asJava
+
+      override def getOrder: Int = org.springframework.core.Ordered.HIGHEST_PRECEDENCE
+
+      private def rule[TOriginal: ClassTag : TypeTag, TAlternate: ClassTag]: AlternateTypeRule = {
+        val original = classTag[TOriginal].runtimeClass
+        val alternate = classTag[TAlternate].runtimeClass
+        val arity = typeOf[TOriginal] match {
+          case ExistentialType(_, TypeRef(_, _, args)) => args.length
+          case TypeRef(_, _, args) => args.length
+        }
+        val wildcards = 1.to(arity).map(_ => classOf[WildcardType]).toArray
+        AlternateTypeRules.newRule(
+          typeResolver.resolve(original, wildcards: _*),
+          typeResolver.resolve(alternate, wildcards: _*)
+        )
+      }
+    }
+  }
+}
